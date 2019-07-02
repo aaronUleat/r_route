facde018395e2bfbba6dc88053f30a081e517ce1
Aaron Ulate <aaronulate@localhost.localdomain>
2019-07-01 22:33:56 -0600

    added recipe module

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,9 +2,9 @@ import React from "react";
 
 const Form = props => (
   <div>
-        <form onSubmit={props.getRecipe}>
-            <input type="text" name="recipeName" />  
-            <button> Search </button>
+        <form style={{ marginBottom: "2rem" }} onSubmit={props.getRecipe}>
+            <input className="form__input" type="text" name="recipeName" />  
+            <button className="form__button"> Search </button>
         </form>
   </div>
 );
--- src/components/Recipes.js
+++ src/components/Recipes.js
@@ -1,16 +1,30 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 const Recipes = ({recipes}) => (
-    <div>
-        <h3> Recipes </h3>
+    <div className="container">
+      <div className="row">
         {recipes.map( recipe => {
-          return (
-            <div key={recipe.recipe_id}>
-               <img  src={recipe.image_url} /> 
-               <p> {recipe.title} </p>
-            </div>
-          )   
-        })}
+            return (
+              <div className="col-md-4 mb-2" key={recipe.recipe_id}>
+                  <div className="recipes__box">
+                    <img className="recipe__box-img" src={recipe.image_url} alt={recipe.title} />
+                    <div className="recipe__text">
+                      <h5 className="recipes__title"> 
+                        {recipe.title.length < 20 ? `${recipe.title}` : `${recipe.title.substring(0, 25)}...` } 
+                      </h5>
+                      <p className="recipes_subtitle"> Publisher: <span> {recipe.publisher} </span>  </p>
+                    </div>  
+                    <button className="recipe_buttons"> 
+                      <Link to={{
+                        pathname: `/recipe/${recipe.recipe_id}`,
+                        state: { recipe: recipe.title }
+                        }}> View Recipe  </Link>
+                    </button>
+                  </div>
+              </div>
+            )   
+          })}
+      </div>
     </div>    
 );
 
